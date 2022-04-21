0e7a06a8ae0dabef8b44be47e7525b067d9dcdac
onomeh <onwuanibeonome@gmail.com>
2022-04-21 18:56:38 +0000

    Update files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import displayPopup from '../modules/popup.js';
 
 const showApi = 'https://api.tvmaze.com/search/shows?q=girls';
 const movieContainer = document.querySelector('.movie_list');
@@ -11,18 +12,21 @@ window.onload = function () {
     allShows.forEach((e) => {
       const allMovies = document.createElement('div');
       allMovies.className = 'movie';
-      allMovies.innerHTML = `
-            <div>
-            <img src="${e.show.image.original}" alt="${e.show.name}">
-            <p>Title: ${e.show.name}</p>
-            <div class="counter">
-                <i class="fa fa-thumbs-up" aria-hidden="true"></i>
-                <i class="fa fa-comment" aria-hidden="true"></i>
-            </div>
-            </div>
-            `;
+      if (e.show.image !== null) {
+        allMovies.innerHTML = `
+        <div>
+        <img src="${e.show.image.original}" alt="Movie">
+        <p>Title: ${e.show.name}</p>
+        <div class="counter">
+            <div class="likes" id="${e.show.id}"><i class="fa fa-thumbs-up" aria-hidden="true"></i> </div>
+            <div><i class="fa fa-comment" aria-hidden="true"></i></div>
+        </div>
+        </div>
+        `;
+      }
       movieContainer.appendChild(allMovies);
     });
+    displayPopup();
   };
   return fetchShows();
 };
