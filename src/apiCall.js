e92e2cd3396e1bae2bf77f2878da0e1b808a0d62
onomeh <onwuanibeonome@gmail.com>
2022-04-22 17:42:52 +0000

    Create call for post and get comment

--- src/apiCall.js
+++ src/apiCall.js
@@ -4,4 +4,30 @@ const fetchShow = async (id) => {
   const allShows = res.json();
   return allShows;
 };
-export default fetchShow;
+
+const postComment = async (id, username, comment) => {
+  const res = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/wBpjy3Zy3kfv5qbqC9Ro/comments', {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: id,
+      username,
+      comment,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  return res.status;
+};
+
+const getComments = async (id) => {
+  const res = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/wBpjy3Zy3kfv5qbqC9Ro/comments?item_id=${id}`);
+  try {
+    const result = res.json();
+    return result;
+  } catch (err) {
+    return [];
+  }
+};
+
+export { fetchShow, postComment, getComments };
