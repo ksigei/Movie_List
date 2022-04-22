26338edad9208a31bf11979671c96713d15dfcf2
onomeh <onwuanibeonome@gmail.com>
2022-04-22 17:43:37 +0000

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import './style.css';
-import displayPopup from '../modules/popup.js';
+import { displayPopup, closePopup } from '../modules/popup.js';
 
 const showApi = 'https://api.tvmaze.com/search/shows?q=girls';
 const movieContainer = document.querySelector('.movie_list');
 
-window.onload = function () {
-  const fetchShows = async () => {
-    const res = await fetch(showApi);
-    const allShows = await res.json();
+const fetchShows = async () => {
+  const res = await fetch(showApi);
+  const allShows = await res.json();
 
-    allShows.forEach((e) => {
-      const allMovies = document.createElement('div');
-      allMovies.className = 'movie';
-      if (e.show.image !== null) {
-        allMovies.innerHTML = `
+  allShows.forEach((e) => {
+    const allMovies = document.createElement('div');
+    allMovies.className = 'movie';
+    if (e.show.image !== null) {
+      allMovies.innerHTML = `
         <div>
-          <img src="${e.show.image.original}" alt="Movie">
+          <img src="${e.show.image.medium}" alt="Movie">
           <p>Title: ${e.show.name}</p>
           <div class="counter">
               <div class="likes" id="${e.show.id}"><i class="fa fa-thumbs-up" aria-hidden="true"></i> </div>
@@ -23,10 +22,10 @@ window.onload = function () {
           </div>
         </div>
         `;
-      }
-      movieContainer.appendChild(allMovies);
-    });
-    displayPopup();
-  };
-  return fetchShows();
+    }
+    movieContainer.appendChild(allMovies);
+  });
+  displayPopup();
+  closePopup();
 };
+fetchShows();
