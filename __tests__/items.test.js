2bf5ef7f86d26e1422cf82332cb434449d5ffe99
Ksigei <sigeikiprono4@gmail.com>
2022-04-23 13:54:02 +0300

    Solve linter errors

--- __tests__/items.test.js
+++ __tests__/items.test.js
@@ -1,12 +1,10 @@
 const showsArr = [
-  { name: "title", id: 'id'},
-  { name: "title", id: 'id'},
-  { name: "title",  id: 'id'},
+  { name: 'title', id: 'id' },
+  { name: 'title', id: 'id' },
+  { name: 'title', id: 'id' },
 ];
 
-const showsCount = (arr) => {
- return arr.length
-}
-describe('Show counter ', () => { 
-    test('Count Number of Shows for All', () => { expect(showsCount(showsArr)).toBe(3); })
-})
+const showsCount = (arr) => arr.length;
+describe('Show counter ', () => {
+  test('Count Number of Shows for All', () => { expect(showsCount(showsArr)).toBe(3); });
+});
